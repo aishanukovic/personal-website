a6573ab6a757fdb6f7d0e01738c42c7b1317fd43
aishanukovic <aishanukovic@gmail.com>
2024-11-13 14:26:35 -0800

    Added scroll to top button

--- PersonalWebsiteJS.js
+++ PersonalWebsiteJS.js
@@ -8,7 +8,25 @@ function typeWriter() {
         header.innerHTML += text.charAt(index);
         index++;
         setTimeout(typeWriter, speed);
-    }
-}
+    };
+};
 
 window.onload = typeWriter;
+
+let scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollTopBtn.style.display = "block";
+    } else {
+        scrollTopBtn.style.display = "none";
+    };
+};
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+};
